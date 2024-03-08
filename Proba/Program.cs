af248a5effaeda335ac92ef0f1af3f46b6f3eae2
TsvetiTsvet999666 <159997613+TsvetiTsvet999666@users.noreply.github.com>
2024-03-08 15:06:15 +0200

    Probvam

---
+++ Proba/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Probvam da izpratq v github");
+        }
+    }
+}
